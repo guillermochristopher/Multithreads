6dfd0475e22c71e504d0c6057e19399dc57c87d1
Guillermo Guillen <guillenazua@gmail.com>
2022-10-25 20:44:14 +0100

    finishing2

--- multithread.java
+++ multithread.java
@@ -28,11 +28,24 @@ class UseThreads {
     public static void main (String args[]){
         System.out.println("Main thread starting");
     
-
+//First, construct a multithread object
     multithread mt = new multithread("Child1");
 
+//next construct a thread from that object
     Thread newThread = new Thread(mt);
-    
+ 
+//Run the execution of the thread
     newThread.start();
+    
+
+    for (int i=0; i<50; i++){
+        System.out.println(".");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException exc) {
+            System.out.println("Main thread interrupted");
+        }
     }
+    System.out.println("Main thread ending.");
 } 
+}
