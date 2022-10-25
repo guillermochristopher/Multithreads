ba451fe2074c0fa6c5f87b7879a26c89d77acdc1
Guillermo Guillen <guillenazua@gmail.com>
2022-10-25 19:49:38 +0100

    Added second class

--- multithread.java
+++ multithread.java
@@ -1,13 +1,14 @@
 //Creating a thread by Implementing Runnable.
 
-class multithread implements Runnable {
-    String thrdName;
+class multithread implements Runnable {  //Objects of this class can be runn
+    String thrdName;                    //in their own threads coz this class
+                                        //implements "Runnable"
     
     multithread(String name){
         thrdName = name;
     }
     //Entry point of thread
-    public void run(){
+    public void run(){     //threating start executing here
         System.out.println(thrdName + " starting.");
         try {
             for(int count=0; count < 10; count++){
@@ -22,3 +23,10 @@ class multithread implements Runnable {
     }
     
 } //asdasd
+
+class UseThreads {
+    public static void main (String args[]){
+        System.out.println("Main thread starting");
+    }
+
+}
